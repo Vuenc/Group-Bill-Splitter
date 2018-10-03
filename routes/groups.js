ec2a6503de08279f708a40476bfdb0f946ac092b
vuenc <=v.buergin@gmx.de>
2018-10-03 12:26:30 +0100

    code skeleton for a node.js app with skeleton files for first routes

---
+++ routes/groups.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let router = express.Router();
+// TODO let mongoose = require('mongoose');
+
+// TODO load model
+
+// TODO mongoose.connect(...);
+// TODO let db = mongoose.connection;
+
+// TODO db connection events
+
+// TODO insert routes
+
+module.exports = router;
