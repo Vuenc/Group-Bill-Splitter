9333e3e4e7a7e4c38483d5a79d048a5e0559599f
vuenc <=v.buergin@gmx.de>
2018-10-08 20:01:43 +0100

    changed groupMembers route to use mongoose's promise-like .then() instead of callbacks

--- routes/groupMembers.js
+++ routes/groupMembers.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let router = express.Router();
-let mongoose = require('mongoose');
 
 require('../models/mongooseConnection');
 let GroupEvent = require('../models/groupEvents');
@@ -9,49 +8,49 @@ let GroupMember = require('../models/groupMembers');
 router.getAll = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    // TODO make sure group event exists?
-    GroupMember.find({groupEventId: req.params.groupEventId}, (err, groupMembers) => {
-        if(err)
-            res.send(err);
-        else
-            res.send(groupMembers); // TODO JSON.stringify?
-    })
+    GroupEvent.find({_id: req.params.groupEventId})
+        .then(groupEvent => {
+            if(groupEvent.length === 0) throw {message: "Group event with id " + req.params.groupEventId + " not found!"};
+            return GroupMember.find({groupEventId: req.params.groupEventId});
+        })
+        .then(groupMember => res.send(groupMember))
+        .catch(err => res.send(err));
+
 };
 
 router.getOne = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    // TODO make sure group event exists?
-    // TODO error handling
-    // TODO don't use findOne
-    GroupMember.findOne({_id: req.params.id, groupEventId: req.params.groupEventId}, (err, groupMember) => {
-        if(err)
-            res.send(err);
-        else
-            res.send(groupMember); // TODO JSON.stringify?
-    })
+    GroupEvent.find({_id: req.params.groupEventId})
+        .then(groupEvent => {
+            if(groupEvent.length === 0) throw {message: "Group event with id " + req.params.groupEventId + " not found!"};
+            return GroupMember.find({_id: req.params.id, groupEventId: req.params.groupEventId});
+        })
+        .then(groupMember => {
+            if(groupMember.length === 0) throw {message: "Group member with id " + req.params.id + " not found!"};
+            res.send(groupMember)
+        })
+        .catch(err => res.send(err)); // TODO send different response code
 };
 
 router.addGroupMember = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    // TODO does not actually ensure that a group event exists!
-    GroupEvent.findById(req.params.groupEventId, (err, groupEvent) => {
-        if(err)
-            res.send(err);
-        else {
+    // TODO validate all necessary data present
+    GroupEvent.find({_id: req.params.groupEventId})
+        .then(groupEvent => {
+            if(groupEvent.length === 0) throw {message: "Group event with id " + req.params.groupEventId + " not found!"};
+
             let groupMember = new GroupMember();
             groupMember.groupEventId = req.params.groupEventId;
             groupMember.name = req.body.name;
             groupMember.summedBalance = 0;
-            groupMember.save(err => {
-                if(err)
-                    res.send({message: 'Group member not added!', errmsg: err});
-                else
-                    res.send({message: 'Group member added successfully', data: groupMember});
-            });
-        }
-    });
+            return groupMember.save();
+        })
+        .then(groupMember => {
+            res.send({message: 'Group member added successfully', data: groupMember});
+        })
+        .catch(err => res.send(err)); // TODO send different response code; unify message format
 };
 
 module.exports = router;
