1f765e8814e847fd4c108c04ed26574e07432707
vuenc <=v.buergin@gmx.de>
2018-10-20 12:07:50 +0100

    changed .env configuration to use global variables, changed environment variable names

--- models/mongooseConnection.js
+++ models/mongooseConnection.js
@@ -1,11 +1,13 @@
 // Snippet adapted from the dotenv documentation to ensure that values from .env file are not shadowed by global environment variables
 const fs = require('fs');
-const dotenv = require('dotenv');
-const envConfig = dotenv.parse(fs.readFileSync('.env'));
+const dotenv = require('dotenv')
+    .config(); // Needed to load local variables automatically
+const envConfig = process.env;
+    // Way to do load environment variables only from local file: = dotenv.parse(fs.readFileSync('.env'));
 const mongoose = require('mongoose');
 
-let mongodbUri = 'mongodb://' + envConfig.DB_USER
-    + ':' + envConfig.DB_PASSWORD + '@ds223253.mlab.com:23253/' + envConfig.DB_NAME;
+let mongodbUri = 'mongodb://' + envConfig.GBS_MLAB_DB_USER
+    + ':' + envConfig.GBS_MLAB_DB_PASSWORD + '@ds223253.mlab.com:23253/' + envConfig.GBS_MLAB_DB_NAME;
 
 mongoose.connect(mongodbUri, {useNewUrlParser: true});
 let db = mongoose.connection;
